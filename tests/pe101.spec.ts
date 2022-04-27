99574f0a93bf98b3e8f20201dc619daceaf82683
Daniel Hernández Fajardo <alu0101320489@ull.edu.es>
2022-04-27 00:23:35 +0000

    inicio

---
+++ tests/pe101.spec.ts
@@ -0,0 +1,9 @@
+import 'mocha';
+import {expect} from 'chai';
+
+describe('Test clase AddMapReduce', () => {
+    
+    it ('Funcionamiento de reduce Add', () => {
+        expect(0).to.be.deep.equal(0);
+    });
+});
